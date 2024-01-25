13559fbd70ff7a15e974548e4d4ee68486a7151e
arsize <arsizeof3416@gmail.com>
2024-01-25 16:29:56 +0800

    fix: 协议

--- pages/legal/index.vue
+++ pages/legal/index.vue
@@ -83,7 +83,7 @@ onMounted(() => {
             <img class="w-full" :src="slide.pictureUrl" alt="" />
             <div class="sub-text p-5">
               <div class="text-lg font-bold mb-5">{{ slide.title }}</div>
-              <div class="textOVerThree" v-html="slide.content"></div>
+              <div class="textOVerThree">{{ slide.introduction }}...</div>
             </div>
           </div>
         </NuxtLink>
